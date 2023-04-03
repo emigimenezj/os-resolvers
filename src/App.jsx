35496f86e4be8630f4392f5edc0d07ee0d799345
Emilio Giménez <emigimenezj@zohomail.com>
2023-04-02 23:31:41 -0300

    Add temporal conditional render for resolvers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+
 import { Memory } from './Memory';
 import { Scheduling } from './Scheduling';
+
 import './App.css';
 
 function App() {
 
+  const [resolver, setResolver] = useState(false);
+
+  const handleClick = () => setResolver(prev => !prev)
+
   return (
     <>
+      <h1>Resolvers!</h1>
+
+      <button
+        onClick={handleClick}
+      >Cambiar</button>
+
+      {
+        resolver
+        ?
         <Scheduling />
+        :
         <Memory />
+      }
     </>
   );
 }
