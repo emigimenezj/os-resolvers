e74be89319fdd08922dbac9591c0890e54b1616a
Emilio Giménez <emigimenezj@zohomail.com>
2023-04-09 23:49:08 -0300

    Add classname to slot component

--- src/components/Slot.jsx
+++ src/components/Slot.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export function Slot({content = '', msg = null, bg = null}) {
+export function Slot({content = '', msg = null, bg = null, className}) {
 
   const msgFormatted = msg?.split('\n').map((sentence, index, {length}) => {
     return (
@@ -12,7 +12,7 @@ export function Slot({content = '', msg = null, bg = null}) {
   });
   
   return (
-    <td style={bg ? { backgroundColor: bg} : null}>
+    <td style={bg ? { backgroundColor: bg} : null} className={className}>
       {content}
       {msg ? <span>{msgFormatted}</span> : null}
     </td>
