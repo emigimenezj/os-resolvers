4543a7d8b3969198ab7950910b6860b456773202
Emilio Giménez <emigimenezj@zohomail.com>
2023-04-09 23:49:32 -0300

    Add support to SC and LRU comments

--- src/components/MemorySolution.jsx
+++ src/components/MemorySolution.jsx
@@ -24,23 +24,47 @@ MENSAJES QUE QUIERO QUE HAYA:
 */
 const MESSAGES = {
   REQUEST: (id) => `Se recibe una petición en memoria para la página ${id}`,
-  MEM_HIT: (id, hits) => `La página ${id} estaba en memoria, por lo que genera un hit.\nTotal de hits: ${hits}`,
-  MEM_MISS: (id, misses) => `La página ${id} no estaba en memoria, por lo que genera un miss.\nTotal de misses: ${misses}`,
+  MEM_HIT: (id, hits) => `La página ${id} estaba en memoria, por lo que genera un hit\nTotal de hits: ${hits}`,
+  MEM_MISS: (id, misses) => `La página ${id} no estaba en memoria, por lo que genera un miss\nTotal de misses: ${misses}`,
   MEM_NORMAL: (id) => `La página ${id} está en memoria y no fue afectada por ningún cambio`,
   MEM_EMPTY: `Esta ranura representa un espacio de memoria vacío`,
   ORD_FIRST: (id) => `La página ${id} será la próxima en ser desalojada`,
-  ORD_MISS: (id) => `La página ${id} se agrega al final del orden de desalojo`,
-  ORD_NORMAL: (id) => `Este es el orden de desalojo de la página ${id}.\nCuando esté primera, será la siguiente en ser desalojada`,
-  ORD_EMPTY: `Esta ranura representa un espacio de orden disponible`,
+  SC_ORD_FIRST: (id) => `Con el próximo miss, la página ${id} perderá su segunda oportunidad y se deslpazará al final del orden de desalojo`,
   LRU_ORD_HIT: (id) => `Al haber generado un hit, la página ${id} se desplaza al final del orden de desalojo`,
-  SC_ORD: (id) => `La página ${id} no será desalojada caso de generar un miss`,
   SC_ORD_HIT: (id) => `La página ${id} obtiene una segunda oportunidad por haber generado un hit`,
-  SC_ORD_MISS: (id) => `Al perder su segunda oportunidad, la página ${id} se desplaza al final del orden de desalojo`
+  ORD_MISS: (id) => `La página ${id} se agrega al final del orden de desalojo`,
+  ORD_NORMAL: (id) => `Este es el orden de desalojo para la página ${id}\nCuando esté primera, será la siguiente en ser desalojada`,
+  SC_ORD_NORMAL: (id) => `La página ${id} tiene una segunda oportunidad\nCuando esté primera, no será desalojada`,
+  ORD_EMPTY: `Esta ranura representa un espacio de orden disponible`,
+}
+
+const ORD_INFO = {
+  FIFO: {
+    FIRST: MESSAGES.ORD_FIRST,
+    NORMAL: MESSAGES.ORD_NORMAL
+  },
+  LRU: {
+    FIRST: MESSAGES.ORD_FIRST,
+    HIT: MESSAGES.LRU_ORD_HIT,
+    NORMAL: MESSAGES.ORD_NORMAL
+  },
+  SC: {
+    FIRST: MESSAGES.SC_ORD_FIRST,
+    HIT: MESSAGES.SC_ORD_HIT,
+    NORMAL: MESSAGES.SC_ORD_NORMAL
+  }
 }
 
 export function MemorySolution({ solution }) {
 
-  const { memoryRecord, orderRecord, hitsRecord, requestSequence } = solution;
+  const {
+    requestSequence,
+    memoryRecord,
+    orderRecord,
+    scRecord,
+    hitsRecord,
+    type
+  } = solution;
 
   return (
     <tbody>
@@ -66,15 +90,19 @@ export function MemorySolution({ solution }) {
             }
             {
               orderRecord[i].map((page, j) => {
-                if (page === pageRequested && !hitsRecord[i])
-                  return <Slot key={j} content={page} msg={MESSAGES.ORD_MISS(page)} />
-                
-                if (j === 0)
-                  return <Slot key={j} content={page} msg={MESSAGES.ORD_FIRST(page)} />
-                
+                const bg = scRecord[i].includes(page) ? 'yellow' : null;
+
+                if (page === pageRequested) {
+                  return hitsRecord[i] && /LRU|SC/.test(type)
+                    ? <Slot key={j} content={page} msg={ORD_INFO[type].HIT(page)} bg={bg} className={'order-slot'} />
+                    : <Slot key={j} content={page} msg={MESSAGES.ORD_MISS(page)} bg={bg} className={'order-slot'} />
+                }
+
+                if (j === 0) return <Slot key={j} content={page} msg={ORD_INFO[type].FIRST(page)} bg={bg} className={'order-slot'} />
+
                 return page
-                  ? <Slot key={j} content={page} msg={MESSAGES.ORD_NORMAL(page)} />
-                  : <Slot key={j} content={page} msg={MESSAGES.ORD_EMPTY} />
+                  ? <Slot key={j} content={page} msg={ORD_INFO[type].NORMAL(page)} bg={bg} className={'order-slot'} />
+                  : <Slot key={j} content={page} msg={MESSAGES.ORD_EMPTY} className={'order-slot'} />
               })
             }
           </tr>
