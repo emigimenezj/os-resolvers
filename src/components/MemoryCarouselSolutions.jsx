41d25a3401e0c4cdfd54c7cb0749e9eb686f6169
Emilio Giménez <emigimenezj@zohomail.com>
2023-04-10 23:44:56 -0300

    Add hit, miss and hit rate under every solution tagble

--- src/components/MemoryCarouselSolutions.jsx
+++ src/components/MemoryCarouselSolutions.jsx
@@ -2,21 +2,45 @@ import { MemorySolution } from './MemorySolution';
 
 export function MemoryCarouselSolutions({ solutions }) {
 
+  const mcd = (a, b) => b === 0 ? a : mcd(b, a % b);
+
   return (
-    <div className='carousel-solutions-container'>
+    <div className={`carousel-solutions-container`}>
       {
         solutions.map((sol, index) => {
+          const { hitsRecord } = sol;
+
+          const totalHits = hitsRecord.filter(Boolean).length;
+          const totalMisses = hitsRecord.length - totalHits;
+
+          const m = mcd(totalHits, totalMisses);
+          const simplifiedFrac = `${totalHits / m} / ${totalMisses / m}`;
+
+          const hitRate = totalHits
+            ? simplifiedFrac
+            : 0;
+          const hitRateAprox = totalHits
+            ? (totalHits/totalMisses).toFixed(3).replace(/0+$/, '')
+            : '0.0';
+
           return (
-            <table key={index}>
-              <thead>
-                <tr>
-                  <th>Peticiones</th>
-                  <th colSpan={sol.frames}>Registro de memoria</th>
-                  <th colSpan={sol.frames}>Registro de orden</th>
-                </tr>
-              </thead>
-              <MemorySolution solution={sol} />
-            </table>
+            <div key={index} className="fade-in">
+              <table>
+                <thead>
+                  <tr>
+                    <th>PÁG.</th>
+                    <th colSpan={sol.frames}>MEMORIA</th>
+                    <th colSpan={sol.frames}>ORDEN</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <MemorySolution solution={sol} />
+                </tbody>
+              </table>
+              <div>HITS: {totalHits}</div>
+              <div>MISSES: {totalMisses}</div>
+              <div>HIT RATE: {hitRate} ~ ({hitRateAprox})</div>
+            </div>
           );
         })
       }
