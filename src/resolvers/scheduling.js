d598bc54d9ec3fd4ee98d8d028ed0ff44e013060
Emilio Giménez <emigimenezj@zohomail.com>
2023-05-16 07:38:41 -0300

    Update scheduling resolver algorithm to version 1.0

--- src/resolvers/scheduling.js
+++ src/resolvers/scheduling.js
@@ -1,9 +1,9 @@
-const  PROC = {
-  BEFORE_EXEC: 'before',
-  RUNNING: 'run',
-  WAITING: 'wait',
+const PROC = {
+  BEFORE_EXEC: 'B',
+  RUNNING: 'R',
+  WAITING: 'W',
   CTX_SWITCH: 'switch',
-  AFTER_EXEC: 'after'
+  AFTER_EXEC: 'A'
 }
 
 export function resolver({ processes, quantum }) {
@@ -17,37 +17,60 @@ export function resolver({ processes, quantum }) {
     quantum
   }
 
-  while(processes.some(proc => proc.burst !== 0)) {
-
-    processes.forEach((proc, i) => {
-
-      if (proc.arrival < CPU.ms) {
+  const recording = () => {
+    processes.forEach((p, i) => {
+      if (CPU.ms < p.arrival) {
         execRecord[i].push(PROC.BEFORE_EXEC);
         return;
       }
-
-      if (!CPU.ready.includes(proc) && CPU.running !== proc) {
-        if (!CPU.running) {
-          CPU.running = proc;
+      if (CPU.running === p) {
           execRecord[i].push(PROC.RUNNING);
           return;
         }
-        CPU.ready.push(proc);
-        execRecord[i].push(PROC.WAITING);
+      if (!p.burst) {
+        execRecord[i].push(PROC.AFTER_EXEC);
         return;
       }
+      execRecord[i].push(PROC.WAITING);
+    });
+  }
+
+  while (processes.some(p => p.burst !== 0)) {
+
+    console.log("hola?");
+
+    const arrivalProcess = processes.filter(p => p.arrival === CPU.ms);
+    CPU.ready = CPU.ready.concat(arrivalProcess);
 
       if (!CPU.running) {
-          CPU.running = proc;
-          proc.burst--;
-          execRecord[i].push(PROC.RUNNING);
-          return;
+      const hasWaitingProcess = CPU.ready.length;
+      if (!hasWaitingProcess) {
+        recording();
+        CPU.ms++;
+        continue;
       }
 
-      execRecord[i].push(PROC.WAITING);
-      return;
-    });
+      //*** SELECTION CRITERIA ***//
+      const [next] = CPU.ready.splice(0, 1);
+      CPU.running = next;
+    }
 
-    ms++;
+    recording();
+
+    CPU.running.burst--;
+    if (!CPU.running.burst) {
+      CPU.running = null;
+    }
+    // TODO: quantum
+
+    CPU.ms++;
   }
+
+  execRecord.forEach(rec => rec.push(PROC.AFTER_EXEC));
+
+  return {
+    execRecord,
+    processes,
+    quantum
+  };
 }
