1f1fb0adcd2328dc848d7cdcf171f3ad2fdfe0f4
Emilio Giménez <emigimenezj@zohomail.com>
2023-05-15 23:58:22 -0300

    Add first approach for scheduling resolver supporting awaiting and running states

--- src/resolvers/scheduling.js
+++ src/resolvers/scheduling.js
@@ -1,3 +1,53 @@
+const  PROC = {
+  BEFORE_EXEC: 'before',
+  RUNNING: 'run',
+  WAITING: 'wait',
+  CTX_SWITCH: 'switch',
+  AFTER_EXEC: 'after'
+}
+
 export function resolver({ processes, quantum }) {
-  const processesRecord = Array.from(Array(processes))
+
+  const execRecord = Array.from(Array(processes.length), () => []);
+
+  const CPU = {
+    ms: 0,
+    running: null,
+    ready: [],
+    quantum
+  }
+
+  while(processes.some(proc => proc.burst !== 0)) {
+
+    processes.forEach((proc, i) => {
+
+      if (proc.arrival < CPU.ms) {
+        execRecord[i].push(PROC.BEFORE_EXEC);
+        return;
+      }
+
+      if (!CPU.ready.includes(proc) && CPU.running !== proc) {
+        if (!CPU.running) {
+          CPU.running = proc;
+          execRecord[i].push(PROC.RUNNING);
+          return;
+        }
+        CPU.ready.push(proc);
+        execRecord[i].push(PROC.WAITING);
+        return;
+      }
+
+      if (!CPU.running) {
+          CPU.running = proc;
+          proc.burst--;
+          execRecord[i].push(PROC.RUNNING);
+          return;
+      }
+
+      execRecord[i].push(PROC.WAITING);
+      return;
+    });
+
+    ms++;
+  }
 }
